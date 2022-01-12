ae7988b7d34e0d543d20077cb4b08020d52a3224
Liam Richardson <liam@richardsonconcepts.com>
2022-01-11 21:13:54 -0500

    Reading lines

--- ETL/postgres/index.js
+++ ETL/postgres/index.js
@@ -1,4 +1,6 @@
 import parseArgs from '../parseArgs.js';
+import fs from 'fs';
+import readline from 'readline';
 
 const docs = `
 Loads data from a proved CSV file into a postgres db.
@@ -6,7 +8,23 @@ Loads data from a proved CSV file into a postgres db.
 Syntax: node ETL/postgres "path/to/csv" database_name [-h|e #]
 options:
 h     displays this help text
-e     ends the csv file read after # lines
-`
+e #    ends the csv file read after # lines
+`;
 
-console.log(parseArgs(['filePath', 'tableName'], [], {'e': 0}, docs))
+const args = parseArgs(['filePath', 'tableName'], [], { e: 0 }, docs);
+args.e = parseInt(args.e);
+
+const stream = fs.createReadStream(args.filePath);
+const rl = readline.createInterface({
+  input: stream,
+  crlfDelay: Infinity,
+});
+let lineNum = 0;
+for await (const line of rl) {
+  if (args.e > 0 && lineNum >= args.e){
+    rl.close();
+  } else {
+    console.log(lineNum, line)
+  }
+  lineNum++;
+}
