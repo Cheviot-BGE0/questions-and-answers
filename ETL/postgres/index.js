4d4455d6c30760fc4e3f3aaab28de6a0dbd95a84
Liam Richardson <liam@richardsonconcepts.com>
2022-01-11 20:52:09 -0500

    wording

--- ETL/postgres/index.js
+++ ETL/postgres/index.js
@@ -6,7 +6,7 @@ Loads data from a proved CSV file into a postgres db.
 Syntax: node ETL/postgres "path/to/csv" "database_name" [-h|e #]
 options:
 h     displays this help text
-e     exits the csv file read after # lines
+e     ends the csv file read after # lines
 `
 
 console.log(parseArgs(['filePath', 'tableName'], [], {'e': 0}, docs))
