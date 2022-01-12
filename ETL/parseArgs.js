e4602fd373c1dcc6269b1b21cca42ddb0566de72
Liam Richardson <liam@richardsonconcepts.com>
2022-01-11 20:26:34 -0500

    Tidying up, jsdoc

--- ETL/parseArgs.js
+++ ETL/parseArgs.js
@@ -1,9 +1,9 @@
 const rawArgs = process.argv;
 
 /**
- *
- * @param { array } flags flag arguments, defaulting to false
- * @param { object } namedVals arguments expecting a value to follow, where key is the name and val is the default
+ * Parse command line arguments from node, setting defaults and throwing errors where it can.
+ * @param {array} flags Flag arguments, defaulting to false.
+ * @param {object} namedVals Arguments expecting a value to follow, where key is the name and val is the default.
  * @returns an object with a list of arguments, and a namedArgs object containing argument name: value pairs
  */
 export default function parseArgs(flags = [], namedVals = {}) {
@@ -11,18 +11,18 @@ export default function parseArgs(flags = [], namedVals = {}) {
   const namedArgs = {};
   //assign defaults
   Object.assign(namedArgs, namedVals);
-  flags.forEach(flag => namedArgs[flag] = false)
+  flags.forEach((flag) => (namedArgs[flag] = false));
 
   for (let i = 2; i < rawArgs.length; i++) {
     let arg = rawArgs[i];
     if (arg.startsWith('-')) {
       arg = arg.substring(1);
-      if (flags.some(flag => flag === arg)) {
+      if (flags.some((flag) => flag === arg)) {
         namedArgs[arg] = true;
       } else if (namedVals[arg] !== undefined) {
         const argVal = rawArgs[i + 1];
         if (argVal === undefined || argVal.startsWith('-')) {
-          throw errorColored(`margument "${arg}" expects a value`)
+          throw errorColored(`margument "${arg}" expects a value`);
         }
         i += 1;
         namedArgs[arg] = argVal;
@@ -37,7 +37,6 @@ export default function parseArgs(flags = [], namedVals = {}) {
   return { args, namedArgs };
 }
 
-
 function errorColored(str) {
-  return new Error(`\x1b[31${str}\x1b[0m`)
+  return new Error(`\x1b[31${str}\x1b[0m`);
 }
