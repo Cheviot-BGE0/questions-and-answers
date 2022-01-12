1b2154c16b78a41ad32c0eabef5d02ba3ff39323
Liam Richardson <liam@richardsonconcepts.com>
2022-01-11 20:52:43 -0500

    Removed extraneous qotes

--- ETL/postgres/index.js
+++ ETL/postgres/index.js
@@ -3,7 +3,7 @@ import parseArgs from '../parseArgs.js';
 const docs = `
 Loads data from a proved CSV file into a postgres db.
 
-Syntax: node ETL/postgres "path/to/csv" "database_name" [-h|e #]
+Syntax: node ETL/postgres "path/to/csv" database_name [-h|e #]
 options:
 h     displays this help text
 e     ends the csv file read after # lines
