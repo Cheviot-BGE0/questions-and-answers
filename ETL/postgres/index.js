495168879e0a380304bb896edfe24874a4d9aa00
Liam Richardson <liam@richardsonconcepts.com>
2022-01-12 20:46:05 -0500

    Fixed file read bug, added postgres write batching

--- ETL/postgres/index.js
+++ ETL/postgres/index.js
@@ -9,30 +9,27 @@ Loads data from a proved CSV file into a postgres db.
 
 Syntax: node ETL/postgres "path/to/csv" database_name [-h|end #]
 options:
+batch #    Number of entries to enter as a batch
 h          displays this help text
 end #      ends the csv file read after # lines
 overerror  overwrites the error output file, if it exists
 `;
 
-const args = parseArgs(['filePath', 'tableName'], ['overerror'], { end: 0, progress: 0 }, docs);
+const args = parseArgs(
+  ['filePath', 'tableName'],
+  ['overerror'],
+  { end: 0, progress: 0, batch: 500 },
+  docs
+);
 args.end = parseInt(args.end);
 
-const fileStream = fs.createReadStream(args.filePath);
-const rl = readline.createInterface({
-  input: fileStream,
-  crlfDelay: Infinity,
-});
 
-let lineNum = 0;
 let fieldNames;
 let errorLines = 0;
 let writtenLines = 0;
 const errorFileName = `${args.tableName}_errorLines.csv`;
-const startTime = Date.now();
-
-const parseLine = (line) => {
-  return line.split(/,+(?=(?:(?:[^"]*"){2})*[^"]*$)/g);
-};
+const startTime = new Date();
+let batch = [];
 
 if (!args.overerror) {
   await new Promise((resolve, reject) => {
@@ -53,14 +50,25 @@ if (!args.overerror) {
   });
 }
 
-const insertQuery = (line) => {
-  const values = parseLine(line);
+const parseLine = (line) => {
+  return line.split(/,+(?=(?:(?:[^"]*"){2})*[^"]*$)/g);
+};
+
+const insertBatch = () => {
+  let placeholderPlace = 1;
   let valuePlaceholders = [];
-  for (let i = 0; i < values.length; i++) {
-    valuePlaceholders.push(`$${i + 1}`);
-  }
-  valuePlaceholders = valuePlaceholders.join(', ');
-  const query = `insert into ${args.tableName} (${fieldNames}) values (${valuePlaceholders})`;
+  const values = [];
+  batch.forEach((line) => {
+    const linePlaceholders = [];
+    const parsed = parseLine(line);
+    parsed.forEach((value) => {
+      values.push(value);
+      linePlaceholders.push(`$${placeholderPlace++}`);
+    });
+    valuePlaceholders.push(`\n(${linePlaceholders.join(', ')})`);
+    return;
+  });
+  const query = `insert into ${args.tableName} (${fieldNames}) values ${valuePlaceholders}`;
   return client.query(query, values);
 };
 
@@ -72,18 +80,24 @@ const incrementQuery = (id) => {
 //ensure nextval exists
 await client.query(`select nextval('public.${args.tableName}_id_seq')`);
 
+const fileStream = fs.createReadStream(args.filePath);
+const rl = readline.createInterface({
+  input: fileStream,
+  crlfDelay: Infinity,
+});
+
+let lineNum = -1;
+
 //read the file
 for await (const line of rl) {
-  //maintain current line number regardless of how many times it's incremented later
-  const currentLine = lineNum;
   lineNum++;
-  if (currentLine  && currentLine % 500 === 0) {
+  if (lineNum && lineNum % 500 === 0) {
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
-    process.stdout.write(`current line: ${currentLine}, errors: ${errorLines}`)
+    process.stdout.write(`current line: ${lineNum}, errors: ${errorLines}`);
   }
 
-  if (currentLine === 0) {
+  if (lineNum === 0) {
     const fields = line.split(',');
     //TODO: format fields entries with pg-format to avoid sql injection
     fieldNames = fields.join(', ');
@@ -94,34 +108,45 @@ for await (const line of rl) {
         resolve();
       });
     });
-  } else if (args.end > 0 && currentLine > args.end) {
-    //TODO: close isn't enough to end the reading.
+  } else if (args.end > 0 && lineNum === args.end) {
     rl.close();
     fileStream.close();
-  } else {
+  } else if (!args.end || lineNum < args.end) {
+
+    batch.push(line);
     try {
-      await insertQuery(line);
-      await incrementQuery(currentLine);
-      writtenLines++;
+      if (batch.length === args.batch) {
+        await insertBatch();
+        batch = [];
+        writtenLines += args.batch;
+      }
     } catch (err) {
-      errorLines++;
+      errorLines += args.batch;
+
       await new Promise((resolve, reject) => {
-        fs.appendFile(errorFileName, `\n${line}`, (err) => {
+        fs.appendFile(errorFileName, `\n${batch.join('\n')}`, (err) => {
           if (err) return reject(err);
           resolve();
         });
       });
+      batch = [];
     }
   }
 }
 
+try{
+  await incrementQuery(lineNum)
+} catch(err) {
+  console.error('entries were written, but auto-increment id was unable to be updated!')
+}
+
+
 console.log(
   `
-  write complete after ${Date.now() - startTime} ms
+  write complete after ${(new Date() - startTime) / 1000} s
   lines written successfully: ${writtenLines}
   lines with errors:          ${errorLines}`
 );
-
 
 //This is hacky, something isn't detaching right
 process.exit();
