dc0bcb143cb1ae542d61978a4df91e1610bada17
Liam Richardson <liam@richardsonconcepts.com>
2022-01-19 14:59:01 -0500

    Placehoders for all path tests

--- spec/server/paths.spec.js
+++ spec/server/paths.spec.js
@@ -1,5 +1,28 @@
 const paths = require('../../server/paths.js');
 
+const success = () => 'success';
+const dbStub = {
+  init: success,
+  getQuestions: success,
+  getAnswers: success,
+  addQuestion: success,
+  addAnswer: success,
+  markQuestionHelpful: success,
+  reportQuestion: success,
+  markAnswerHelpful: success,
+  reportAnswer: success,
+};
+
 describe('Server Paths', () => {
-  beforeAll()
-})
+  beforeAll(() => {
+    paths.init(dbStub);
+  });
+  describe('GET questions', async () => {});
+  describe('GET answers', async () => {});
+  describe('POST question', async () => {});
+  describe('POST answer', async () => {});
+  describe('PUT questions helpful', async () => {});
+  describe('PUT questions report', async () => {});
+  describe('PUT answers helpful', async () => {});
+  describe('PUT answers report', async () => {});
+});
