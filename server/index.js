4bc8576f104bb25d2b769b9b77ce0fe1be49c682
Liam Richardson <liam@richardsonconcepts.com>
2022-01-22 12:46:56 -0500

    Fixed paths

--- server/index.js
+++ server/index.js
@@ -7,21 +7,21 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get('/questions', paths.getQuestions);
+app.get('/qa/questions', paths.getQuestions);
 
-app.get('/questions/:question_id/answers', paths.getAnswers);
+app.get('/qa/questions/:question_id/answers', paths.getAnswers);
 
-app.post('/questions', paths.postQuestion)
+app.post('/qa/questions', paths.postQuestion)
 
-app.post('/questions/:question_id/answers', paths.postAnswer)
+app.post('/qa/questions/:question_id/answers', paths.postAnswer)
 
-app.put('/questions/:question_id/helpful', paths.putQuestionHelpful)
+app.put('/qa/questions/:question_id/helpful', paths.putQuestionHelpful)
 
-app.put('/questions/:question_id/report', paths.putQuestionReport)
+app.put('/qa/questions/:question_id/report', paths.putQuestionReport)
 
-app.put('/answers/:answer_id/helpful', paths.putAnswerHelpful)
+app.put('/qa/answers/:answer_id/helpful', paths.putAnswerHelpful)
 
-app.put('/answers/:answer_id/report', paths.putAnswerReport)
+app.put('/qa/answers/:answer_id/report', paths.putAnswerReport)
 
 app.use(express.static('static'))
 
